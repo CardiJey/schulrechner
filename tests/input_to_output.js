d161461374963db724e9686770f21fb0e8986cbf
CardiJey <joris.scholl@posteo.de>
2025-06-07 00:28:37 +0200

    Added more Tests

--- tests/input_to_output.js
+++ tests/input_to_output.js
@@ -217,6 +217,515 @@ const tests = [
         ],
         "rendered_input": "<span class=\"pow_bottom\">(150238)</span><span class=\"pow_top\">2</span> ",
         "rendered_output": "2.257145664e+10"
+    },
+    {
+        "name": "pow n test",
+        "input_history": [
+            "key_1",
+            "key_2",
+            "key_comma",
+            "key_5",
+            "key_pown",
+            "key_1",
+            "key_comma",
+            "key_7",
+            "key_="
+        ],
+        "rendered_input": "<span class=\"pow_bottom\">(12,5)</span><span class=\"pow_top\">1,7</span> ",
+        "rendered_output": "73.23977849"
+    },
+    {
+        "name": "log test",
+        "input_history": [
+            "key_log",
+            "key_1",
+            "key_6",
+            "key_comma",
+            "key_8",
+            "key_)",
+            "key_="
+        ],
+        "rendered_input": "log(16,8) ",
+        "rendered_output": "1.225309282"
+    },
+    {
+        "name": "ln test",
+        "input_history": [
+            "key_ln",
+            "key_9",
+            "key_9",
+            "key_9",
+            "key_9",
+            "key_)",
+            "key_="
+        ],
+        "rendered_input": "ln(9999) ",
+        "rendered_output": "9.210240367"
+    },
+    {
+        "name": "pow-1 test",
+        "input_history": [
+            "key_4",
+            "key_5",
+            "key_3",
+            "key_comma",
+            "key_2",
+            "key_9",
+            "key_pow-1",
+            "key_="
+        ],
+        "rendered_input": "<span class=\"pow_bottom\">(453,29)</span><span class=\"pow_top\">-1</span> ",
+        "rendered_output": "<span class=\"frac_wrapper\"><span class=\"frac_top\">100</span><span class=\"frac_bottom\">45329</span></span>"
+    },
+    {
+        "name": "log n test",
+        "input_history": [
+            "key_logn",
+            "key_9",
+            "key_8",
+            "key_comma",
+            "key_5",
+            "key_dir2",
+            "key_8",
+            "key_5",
+            "key_4",
+            "key_2",
+            "key_9",
+            "key_="
+        ],
+        "rendered_input": "log<span class=\"logn_bottom\">98,5</span>(85429) ",
+        "rendered_output": "2.473921788"
+    },
+    {
+        "name": "third root test",
+        "input_history": [
+            "key_sqrt3",
+            "key_4",
+            "key_5",
+            "key_comma",
+            "key_9",
+            "key_="
+        ],
+        "rendered_input": "<span class=\"pow_top\">3</span><span class=\"sqrt_wrapper\"><span class=\"scale_height\">√</span><span class=\"sqrt\">45,9</span></span> ",
+        "rendered_output": "3.580449576"
+    },
+    {
+        "name": "pow n test",
+        "input_history": [
+            "key_3",
+            "key_2",
+            "key_comma",
+            "key_4",
+            "key_pow3",
+            "key_="
+        ],
+        "rendered_input": "<span class=\"pow_bottom\">(32,4)</span><span class=\"pow_top\">3</span> ",
+        "rendered_output": "34012.224"
+    },
+    {
+        "name": "INSERT_TEST_NAME",
+        "input_history": [
+            "key_sqrtn",
+            "key_7",
+            "key_dir2",
+            "key_4",
+            "key_2",
+            "key_6",
+            "key_8",
+            "key_comma",
+            "key_2",
+            "key_5",
+            "key_="
+        ],
+        "rendered_input": "<span class=\"pow_top\">7</span><span class=\"sqrt_wrapper\"><span class=\"scale_height\">√</span><span class=\"sqrt\">4268,25</span></span> ",
+        "rendered_output": "3.300708118"
+    },
+    {
+        "name": "integer faculty test",
+        "input_history": [
+            "key_8",
+            "key_faculty",
+            "key_="
+        ],
+        "rendered_input": "8! ",
+        "rendered_output": "40320"
+    },
+    {
+        "name": "INSERT_TEST_NAME",
+        "input_history": [
+            "key_epow",
+            "key_8",
+            "key_="
+        ],
+        "rendered_input": "<span class=\"pow_bottom\">(e)</span><span class=\"pow_top\">8</span> ",
+        "rendered_output": "2980.957987"
+    },
+    {
+        "name": "pi test",
+        "input_history": [
+            "key_pi",
+            "key_="
+        ],
+        "rendered_input": "π ",
+        "rendered_output": "3.141592654"
+    },
+    {
+        "name": "INSERT_TEST_NAME",
+        "input_history": [
+            "key_e",
+            "key_="
+        ],
+        "rendered_input": "e ",
+        "rendered_output": "2.718281828"
+    },
+    {
+        "name": "pow 10 integer test",
+        "input_history": [
+            "key_4",
+            "key_2",
+            "key_pow10",
+            "key_6",
+            "key_="
+        ],
+        "rendered_input": "42<span class=\"pow10\">×⒑</span>6 ",
+        "rendered_output": "42000000"
+    },
+    {
+        "name": "ans test",
+        "input_history": [
+            "key_4",
+            "key_+",
+            "key_2",
+            "key_=",
+            "key_Ans",
+            "key_+",
+            "key_8",
+            "key_="
+        ],
+        "rendered_input": "Ans+8 ",
+        "rendered_output": "14"
+    },
+    {
+        "name": "auto ans test",
+        "input_history": [
+            "key_5",
+            "key_+",
+            "key_7",
+            "key_=",
+            "key_x",
+            "key_3",
+            "key_="
+        ],
+        "rendered_input": "Ans×3 ",
+        "rendered_output": "36"
+    },
+    {
+        "name": "order of basic operations test",
+        "input_history": [
+            "key_1",
+            "key_2",
+            "key_+",
+            "key_4",
+            "key_x",
+            "key_5",
+            "key_-",
+            "key_8",
+            "key_÷",
+            "key_3",
+            "key_+",
+            "key_7",
+            "key_="
+        ],
+        "rendered_input": "12+4×5-8÷3+7 ",
+        "rendered_output": "36.33333333"
+    },
+    {
+        "name": "brackets test",
+        "input_history": [
+            "key_5",
+            "key_+",
+            "key_(",
+            "key_1",
+            "key_2",
+            "key_+",
+            "key_4",
+            "key_)",
+            "key_x",
+            "key_8",
+            "key_="
+        ],
+        "rendered_input": "5+(12+4)×8 ",
+        "rendered_output": "133"
+    },
+    {
+        "name": "implicit multiplication test",
+        "input_history": [
+            "key_1",
+            "key_2",
+            "key_shift",
+            "key_pow10",
+            "key_+",
+            "key_5",
+            "key_(",
+            "key_3",
+            "key_-",
+            "key_7",
+            "key_)",
+            "key_-",
+            "key_8",
+            "key_log",
+            "key_4",
+            "key_)",
+            "key_="
+        ],
+        "rendered_input": "12π+5(3-7)-8log(4) ",
+        "rendered_output": "12.88263191"
+    },
+    {
+        "name": "sin cos tan test",
+        "input_history": [
+            "key_sin",
+            "key_1",
+            "key_4",
+            "key_)",
+            "key_-",
+            "key_cos",
+            "key_3",
+            "key_2",
+            "key_)",
+            "key_+",
+            "key_tan",
+            "key_8",
+            "key_)",
+            "key_="
+        ],
+        "rendered_input": "sin(14)-cos(32)+tan(8) ",
+        "rendered_output": "-0.4655853659"
+    },
+    {
+        "name": "user variables test",
+        "input_history": [
+            "key_4",
+            "key_5",
+            "key_+",
+            "key_8",
+            "key_shift",
+            "key_rcl",
+            "key_(-)",
+            "key_8",
+            "key_-",
+            "key_4",
+            "key_alpha",
+            "key_(-)",
+            "key_shift",
+            "key_rcl",
+            "key_°",
+            "key_alpha",
+            "key_(-)",
+            "key_+",
+            "key_alpha",
+            "key_°",
+            "key_-",
+            "key_alpha",
+            "key_SD",
+            "key_="
+        ],
+        "rendered_input": "A+B-Y ",
+        "rendered_output": "-151"
+    },
+    {
+        "name": "inline cursor movement test",
+        "input_history": [
+            "key_1",
+            "key_4",
+            "key_dir0",
+            "key_7",
+            "key_dir2",
+            "key_3",
+            "key_5",
+            "key_dir0",
+            "key_dir0",
+            "key_dir0",
+            "key_8",
+            "key_-",
+            "key_dir2",
+            "key_dir2",
+            "key_dir2",
+            "key_dir2",
+            "key_dir2",
+            "key_5",
+            "key_dir0",
+            "key_2",
+            "key_dir0",
+            "key_dir0",
+            "key_dir0",
+            "key_dir0",
+            "key_dir0",
+            "key_7",
+            "key_="
+        ],
+        "rendered_input": "12578-4735 ",
+        "rendered_output": "7843"
+    },
+    {
+        "name": "fraction cursor movement test",
+        "input_history": [
+            "key_frac",
+            "key_7",
+            "key_+",
+            "key_frac",
+            "key_9",
+            "key_-",
+            "key_3",
+            "key_dir1",
+            "key_5",
+            "key_+",
+            "key_frac",
+            "key_3",
+            "key_dir1",
+            "key_7",
+            "key_dir3",
+            "key_dir1",
+            "key_dir1",
+            "key_7",
+            "key_-",
+            "key_frac",
+            "key_8",
+            "key_dir1",
+            "key_4",
+            "key_6",
+            "key_dir0",
+            "key_dir0",
+            "key_dir0",
+            "key_dir0",
+            "key_dir0",
+            "key_dir3",
+            "key_dir0",
+            "key_dir0",
+            "key_dir0",
+            "key_dir0",
+            "key_dir0",
+            "key_dir0",
+            "key_dir3",
+            "key_2",
+            "key_="
+        ],
+        "rendered_input": "<span class=\"alignLeft120\"></span><span class=\"frac_wrapper\" style=\"vertical-align: calc(-17.1625px + 0.06rem);\"><span class=\"frac_top\">7+<span class=\"alignLeft121\"></span><span class=\"frac_wrapper\" style=\"vertical-align: calc(-20.3625px + 0.06rem);\"><span class=\"frac_top\">9-32</span><span class=\"frac_bottom alignRight121\">5+<span class=\"alignLeft122\"></span><span class=\"frac_wrapper\" style=\"vertical-align: calc(-9.7875px + 0.06rem);\"><span class=\"frac_top\">3</span><span class=\"frac_bottom alignRight122\">7</span></span></span></span></span><span class=\"frac_bottom alignRight120\">7-<span class=\"alignLeft123\"></span><span class=\"frac_wrapper\" style=\"vertical-align: calc(-9.7875px + 0.06rem);\"><span class=\"frac_top\">8</span><span class=\"frac_bottom alignRight123\">46</span></span></span></span> ",
+        "rendered_output": "<span class=\"frac_wrapper\"><span class=\"frac_top\">2415</span><span class=\"frac_bottom\">5966</span></span>"
+    },
+    {
+        "name": "block inclusion in container operations",
+        "input_history": [
+            "key_7",
+            "key_8",
+            "key_frac",
+            "key_3",
+            "key_dir2",
+            "key_-",
+            "key_7",
+            "key_8",
+            "key_5",
+            "key_+",
+            "key_4",
+            "key_frac",
+            "key_(",
+            "key_3",
+            "key_-",
+            "key_4",
+            "key_)",
+            "key_frac",
+            "key_4",
+            "key_dir2",
+            "key_dir2",
+            "key_-",
+            "key_(",
+            "key_3",
+            "key_x",
+            "key_2",
+            "key_)",
+            "key_pow2",
+            "key_frac",
+            "key_7",
+            "key_="
+        ],
+        "rendered_input": "<span class=\"alignLeft62\"></span><span class=\"frac_wrapper\" style=\"vertical-align: calc(-6.58749px + 0.06rem);\"><span class=\"frac_top\">78</span><span class=\"frac_bottom alignRight62\">3</span></span>-785+<span class=\"alignLeft63\"></span><span class=\"frac_wrapper\" style=\"vertical-align: calc(-17.1625px + 0.06rem);\"><span class=\"frac_top\">4</span><span class=\"frac_bottom alignRight63\"><span class=\"alignLeft64\"></span><span class=\"frac_wrapper\" style=\"vertical-align: calc(-9.7875px + 0.06rem);\"><span class=\"frac_top\">(3-4)</span><span class=\"frac_bottom alignRight64\">4</span></span></span></span>-<span class=\"alignLeft65\"></span><span class=\"frac_wrapper\" style=\"vertical-align: calc(-6.58749px + 0.06rem);\"><span class=\"frac_top\"><span class=\"pow_bottom\">((3×2))</span><span class=\"pow_top\">2</span></span><span class=\"frac_bottom alignRight65\">7</span></span> ",
+        "rendered_output": "<span class=\"frac_wrapper\"><span class=\"frac_top\">-5461</span><span class=\"frac_bottom\">7</span></span>"
+    },
+    {
+        "name": "ac test",
+        "input_history": [
+            "key_4",
+            "key_5",
+            "key_7",
+            "key_8",
+            "key_ac",
+            "key_3",
+            "key_-",
+            "key_5",
+            "key_ac",
+            "key_5",
+            "key_x",
+            "key_8",
+            "key_="
+        ],
+        "rendered_input": "5×8 ",
+        "rendered_output": "40"
+    },
+    {
+        "name": "del integer test",
+        "input_history": [
+            "key_4",
+            "key_5",
+            "key_7",
+            "key_del",
+            "key_9",
+            "key_del",
+            "key_del",
+            "key_6",
+            "key_5",
+            "key_dir0",
+            "key_dir0",
+            "key_del",
+            "key_3",
+            "key_5",
+            "key_dir2",
+            "key_del",
+            "key_="
+        ],
+        "rendered_input": "355 ",
+        "rendered_output": "355"
+    },
+    {
+        "name": "frac deletion",
+        "input_history": [
+            "key_frac",
+            "key_5",
+            "key_4",
+            "key_dir2",
+            "key_5",
+            "key_dir0",
+            "key_dir0",
+            "key_dir0",
+            "key_dir0",
+            "key_del"
+        ],
+        "rendered_input": "<span class=\"cursor\"></span>545 ",
+        "rendered_output": ""
+    }
+]
+
+const known_to_fail_tests = [
+    {
+        "name": "negative third root test",
+        "input_history": [
+            "key_sqrt3",
+            "key_-",
+            "key_2",
+            "key_7",
+            "key_="
+        ],
+        "rendered_input": "<span class=\"pow_top\">3</span><span class=\"sqrt_wrapper\"><span class=\"scale_height\">√</span><span class=\"sqrt\">-27</span></span> ",
+        "rendered_output": "-3"
     }
 ]
 
@@ -230,5 +739,31 @@ for(let test_index = 0; test_index < tests.length; test_index++){
             eval_input_history(this_test.input_history),
             [rendered_input,rendered_output]
         );
+    })
+}
+for(let test_index = 0; test_index < known_to_fail_tests.length; test_index++){
+    let this_test = known_to_fail_tests[test_index]
+    let rendered_input = this_test.rendered_input.replaceAll("\"","'").replace(/\s*style='[^']*'/g, '')
+    let rendered_output = this_test.rendered_output.replaceAll("\"","'").replace(/\s*style='[^']*'/g, '')
+
+    test(this_test.name + " known to fail", (t) => {
+        let passedUnexpectedly = false;
+
+        try {
+            assert.deepStrictEqual(
+                eval_input_history(this_test.input_history),
+                [rendered_input,rendered_output]
+            );
+            passedUnexpectedly = true;
+        } catch (e) {
+            // Known broken, do nothing
+            return;
+        }
+
+        if (passedUnexpectedly) {
+            throw new Error(
+            '❗ ' + this_test.name + ' unexpectedly started working - remove test suppression'
+            );
+        }
     })
 }
