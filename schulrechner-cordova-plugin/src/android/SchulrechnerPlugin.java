e8a9f13e369f1d9a24134c2cb6defd3628a2d288
CardiJey <joris.scholl@posteo.de>
2025-06-28 10:02:44 +0200

    Trying different approach

--- schulrechner-cordova-plugin/src/android/SchulrechnerPlugin.java
+++ schulrechner-cordova-plugin/src/android/SchulrechnerPlugin.java
@@ -1,22 +1,34 @@
 package com.schulrechner.plugin;
 
+import android.util.Log;
 import android.webkit.WebView;
 import android.webkit.WebSettings;
 
-import org.apache.cordova.*;
-import org.json.JSONArray;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaWebView;
 
 public class SchulrechnerPlugin extends CordovaPlugin {
-    @Override
-    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-        super.initialize(cordova, webView);
-        WebView view = (WebView) webView.getEngine().getView();
-        WebSettings settings = view.getSettings();
-        settings.setTextZoom(100);
-    }
+    private static final String LOG_TAG = "SchulrechnerPlugin";
 
     @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
-        return false; // no JS bridge needed
+    public void initialize(final CordovaInterface cordova, final CordovaWebView webView) {
+        super.initialize(cordova, webView);
+
+        cordova.getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    WebView wv = (WebView) webView.getEngine().getView();
+                    WebSettings ws = wv.getSettings();
+
+                    Log.w(LOG_TAG, "[ResetFontSize][loaded] getTextZoom: " + ws.getTextZoom());
+                    ws.setTextZoom(100);
+                    Log.w(LOG_TAG, "[ResetFontSize][updated] getTextZoom: " + ws.getTextZoom());
+                } catch (Exception e) {
+                    Log.e(LOG_TAG, "Error setting WebView text zoom", e);
+                }
+            }
+        });
     }
 }
