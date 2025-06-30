ec58cb65a2a976563d766c12a2fabb71a56f6490
CardiJey <joris.scholl@posteo.de>
2025-06-30 11:49:57 +0200

    Add CONST Input menu

--- www/js/logic.js
+++ www/js/logic.js
@@ -231,7 +231,7 @@ class Frac_Element extends Math_Element{
                 element = element.neighbors[0]
             }
         }else{
-            this.children = [
+            this.global_logic_vars.active_input_handler.children = [
                 new Container_Element(this,"</span><span class='frac_bottom alignRight" + align_id + "'>","][1]/[",this,false)
             ]
         }
@@ -606,6 +606,73 @@ class InputHandler{
     }
 }
 
+class ConstSelectInput extends InputHandler{
+    constructor(display_input_element, math_input_element, display_output_element, math_output_element, parent_handler, global_logic_vars, ui, userLang){
+        super(display_input_element, math_input_element, display_output_element, math_output_element, global_logic_vars, ui, userLang)
+        this.numbers = []
+        this.parent_handler = parent_handler
+        this.max_input = [4,0]
+    }
+
+    // Method to handle input
+    handle(input_code) {
+        switch(input_code){
+                case "key_0":
+                case "key_1":
+                case "key_2":
+                case "key_3":
+                case "key_4":
+                case "key_5":
+                case "key_6":
+                case "key_7":
+                case "key_8":
+                case "key_9":
+                    let number = input_code.substring(4)
+                    let input_allowed = true
+                    this.numbers.push(number)
+                    for(let i = 0; i < this.numbers.length; i++){
+                        if(this.numbers[i] < this.max_input[i]){
+                            break
+                        }else if(this.numbers[i] > this.max_input[i]){
+                            input_allowed = false
+                            break
+                        }
+                    }
+                    if(!input_allowed){
+                        this.numbers.pop()
+                    }
+                break;
+        }
+        if(this.numbers.length >= 2){
+            this.global_logic_vars.active_input_handler = this.parent_handler
+            this.parent_handler.input_code_history.push("key_CONST_" + this.numbers[0] + this.numbers[1])
+        }
+        this.global_logic_vars.active_input_handler.update_display(true);
+        this.global_logic_vars.active_input_handler.update_position()
+    }
+
+    update_display() {
+        this.math_input_element.innerHTML = "KONSTANTE<br>Nummer 01~40?"
+        let out_string = "["
+        for(let i = 0; i < 2; i++){
+            if(this.numbers.length > i){
+                out_string += this.numbers[i]
+            }else{
+                out_string += "_"
+            }
+        }
+        out_string += "]"
+        this.math_output_element.innerHTML = out_string
+        this.math_input_element.scroll(0,0)
+        this.ui.vertical_align_elements()
+    }
+
+    update_position() {
+        this.ui.align_element(this.display_input_element, this.math_input_element);
+        this.ui.align_element(this.display_output_element, this.math_output_element);
+    }
+}
+
 class EquationInputHandler extends InputHandler{
     constructor(display_input_element, math_input_element, display_output_element, math_output_element, parent_handler, global_logic_vars, ui, userLang) {
         super(display_input_element, math_input_element, display_output_element, math_output_element, global_logic_vars, ui, userLang)
@@ -680,9 +747,22 @@ class EquationInputHandler extends InputHandler{
                 this.input_code_history.splice(this.input_code_history.length - 1,0,"end");
                 this.input_code_history.push("key_=");
             }
+            if(this.input_code_history[this.input_code_history.length - 1] == "key_CONST"){
+                this.global_logic_vars.active_input_handler = new ConstSelectInput(
+                    this.display_input_element,
+                    this.math_input_element,
+                    this.display_output_element,
+                    this.math_output_element,
+                    this,
+                    this.global_logic_vars,
+                    this.ui,
+                    this.userLang
+                )
+                this.input_code_history.pop(2)
+            }
         }
-        this.update_display(true);
-        this.update_position()
+        this.global_logic_vars.active_input_handler.update_display(true);
+        this.global_logic_vars.active_input_handler.update_position()
     }
 
     update_display(show_cursor) {
@@ -816,7 +896,12 @@ class EquationInputHandler extends InputHandler{
             ]
             let new_elements = [];
 
-            switch(input_code){
+            let input_code_for_switch = input_code
+            if(input_code_for_switch.startsWith("key_CONST")){
+                input_code_for_switch = "key_CONST"
+            }
+
+            switch(input_code_for_switch){
                 case "key_0":
                 case "key_1":
                 case "key_2":
@@ -898,6 +983,10 @@ class EquationInputHandler extends InputHandler{
                     cursor_element = new_elements[0]
                     break;
 
+                case "key_CONST":
+                    new_elements.push(new Const_Element(cursor_element,parseInt(input_code.substring(10))-1))
+                    cursor_element = new_elements[0]
+                break;
 
                 case "key_=":
                     if(cursor_element.type != "start" || cursor_element.neighbors[2]){
